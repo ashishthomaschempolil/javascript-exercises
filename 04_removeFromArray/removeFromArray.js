0e1aafdcd0fd16b26342e5b46587186a6f78fcc2
ashishthomaschempolil <ashishthomas7@gmail.com>
2022-12-24 18:34:22 -0500

    Solved 4th

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    // Your code here
+    temp  = arr;
+    for (let i=0; i<args.length; i++){
+        temp = temp.filter((item)=>item!==args[i]) //should only remove same type as well, check last test in the other file
+    }
+    return temp
 };
 
 // Do not edit below this line
