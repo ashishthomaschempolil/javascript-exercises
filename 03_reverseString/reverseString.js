de1956bde2de650b8b54ea2451ca6c33cd482c27
ashishthomaschempolil <ashishthomas7@gmail.com>
2022-12-24 18:13:45 -0500

    Finished 3rd problem

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    final = "";
 
+    let i = str.length-1;
+
+    while (i>=0){
+        final+=str[i]
+        i--;
+    }
+    return final
 };
 
 // Do not edit below this line
