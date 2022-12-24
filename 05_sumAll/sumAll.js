c03abd805db1fb6c1d0022ee89720755dda590d5
ashishthomaschempolil <ashishthomas7@gmail.com>
2022-12-24 18:51:50 -0500

    Solved 5th

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (a<0 || b<0 || typeof(a) != "number" || typeof(b) != "number"){
+        return "ERROR"
+    }
+    let final = 0,
+    i = Math.min(a,b);
+    while(i<=Math.max(a,b)){
+        final+=i
+        i++
+    }
+    return final
 };
 
 // Do not edit below this line
