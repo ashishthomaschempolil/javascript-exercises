74ff4c359771bec813fe1758ac982694d751ccb1
ashishthomaschempolil <ashishthomas7@gmail.com>
2022-12-24 18:04:48 -0500

    Finished 1 and 2 problem

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, number) {
+    if (number < 0){
+        return "ERROR";
+    }
 
+    final = "";
+    while (number >0){
+        final += str;
+        number--;
+    }
+    return final;
 };
 
 // Do not edit below this line
