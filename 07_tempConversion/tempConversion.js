004bd922545fddd02b5b8e583811240cd9d7430b
ashishthomaschempolil <ashishthomas7@gmail.com>
2022-12-24 19:19:46 -0500

    Solved 7th

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  result = Math.round(((temp - 32)*5/9)*10)/10
+  return result
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  result = Math.round((1.8*temp + 32)*10)/10 //rounding to 1 decimal place
+  return result
 };
 
 // Do not edit below this line
