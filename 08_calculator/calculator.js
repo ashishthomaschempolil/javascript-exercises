2c0de59bdcf7e366099ca478f9c8791a973796c6
ashishthomaschempolil <ashishthomas7@gmail.com>
2023-01-03 22:17:55 -0500

    Finished 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	if (arr === []){
+    return 0
+  }
+  let final = 0
+  for (n in arr){ //gets the index only
+    final+=arr[n]
+  }
+  return final
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if (arr === []){
+    return 0;
+  }
+  let final = 1;
+  for (n in arr){
+    final*=arr[n]
+  }
+  return final
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a == 0){
+    return 1;
+  }
+  pr = 1
+  for (let i = a; i>0; i--){
+    pr*=i
+  }
+  return pr
 };
 
 // Do not edit below this line
