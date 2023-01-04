74c23032f5815b0e06aaaf83e617f29895fdba34
ashishthomaschempolil <ashishthomas7@gmail.com>
2023-01-03 22:31:40 -0500

    Finished 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+    a = a.toLowerCase().replace(/[^a-z]/g, ""); // remove all non-alphabetic characters
+    let b = ''
+    for (let i=a.length-1; i>=0; i--){
+        b+=a[i]
+    }
+    return a==b;
 };
 
 // Do not edit below this line
