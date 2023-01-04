6fc2b1ae39711f34cfb946483f00bc4febcf89a9
ashishthomaschempolil <ashishthomas7@gmail.com>
2023-01-03 23:37:26 -0500

    Finished 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const comparePeople = (person1, person2) => {
+        //if both person yer of death is defined
+        if (person1.yearOfDeath && person2.yearOfDeath) {
+            person1Age = person1.yearOfDeath - person1.yearOfBirth;
+            person2Age = person2.yearOfDeath - person2.yearOfBirth;
 
+        return person1Age>person2Age ? person1: person2;
+        }
+        else {
+            return person1.yearOfBirth < person2.yearOfBirth ? person1: person2;
+        }
+        
+    }
+
+    maxAge = arr.reduce(comparePeople);
+    return maxAge;
 };
 
 // Do not edit below this line
