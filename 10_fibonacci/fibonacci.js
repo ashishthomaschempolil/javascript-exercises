e30abd7e104aa38caa396efa30189d3ac648cf2f
ashishthomaschempolil <ashishthomas7@gmail.com>
2023-01-03 23:10:29 -0500

    Finished 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (typeof n === 'string'){
+        n = Number(n);
+    }
 
+    if (n<0){
+        return "OOPS"
+    }
+    fib = {
+        1: 1,
+        2: 1
+    }
+
+    if (n>3){
+        for (let i=3; i<=n;i++){
+            fib[i] = fib[i-1] + fib[i-2]
+        }
+    }
+
+
+    return fib[n]
 };
 
 // Do not edit below this line
